46a4d7c3a592688b3057e2ff1481432e6bfccadc
teacherIan <ian_malloy@ssis.asia>
2023-06-04 16:53:41 +0800

    first commit

---
+++ src/lib/Card.svelte
@@ -0,0 +1,30 @@
+<script lang='ts'>
+    // your script goes here
+    export let message:string
+    export let house:string
+    export let index:number
+    // let score: number = 0
+
+    import { localStorageStore } from '@skeletonlabs/skeleton';
+    	import type { Writable } from 'svelte/store';
+
+    const newScore: Writable<[number,number,number,number]> = localStorageStore('newStore', [0,0,0,0])
+
+        console.log($newScore)
+
+
+</script>
+<div class="container flex flex-col justify-center align-center rounded-3xl">
+    <div class="container flex justify-center align-center rounded-3xl ">
+        <button class=' hover:bg-green-400 flex items-center w-20 justify-center text-8xl bg-green-600 text-white rounded-3xl'on:click={()=> $newScore[index] += 1} >+</button>
+        <a href="/" class="card !bg-transparent  rounded-3xl">
+            
+        <img src={message} alt="" srcset="" class= ' h-60 w-60 rounded-3xl opacity-90 object-fit' >
+        </a>
+        <button class=' hover:bg-red-700 flex items-center w-20  justify-center text-9xl bg-red-900 text-white rounded-3xl'  on:click={()=> $newScore[index] -= 1} >-</button>
+    
+    </div>
+    <div class='w-100 bg-white h-20 flex items-center justify-center rounded-3xl text-5xl '  >{$newScore[index]}</div>
+</div>
+
+
---
+++ src/routes/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+// @ts-nocheck
+
+
+	/* grid grid-rows-2 grid-flow-col gap-2*/
+	import Card from "$lib/Card.svelte";
+	// @ts-ignore
+	import sapphire from '$lib/sapphireDark copy.JPG'
+	// @ts-ignore
+	import ruby from '$lib/rubyDark.JPG'
+	// @ts-ignore
+	import amber from '$lib/amberDark copy.JPG'
+	// @ts-ignore
+	import pearl from '$lib/pearlDark copy.JPG'
+
+</script>
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+
+<div class="container h-full w-full mx-auto flex justify-center items-center overflow-hidden">
+	<div class=" grid grid-rows-2 grid-flow-col gap-10 h-90">
+		<Card message={ruby} house='ruby' index=0/>
+		<Card message={amber} house='amber' index=1/>
+		<Card message={pearl} house='pearl' index=2/>
+		<Card message={sapphire} house='sapphire' index=3/>
+		
+	</div>
+	
+</div>
+
+
