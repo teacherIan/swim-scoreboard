07adc542e4e5caee53f1cfe6c44de589fefb6621
teacherIan <ian_malloy@ssis.asia>
2023-06-04 18:01:31 +0800

    xx

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -1,12 +1,20 @@
 <script lang='ts'>
     // your script goes here
-    export let message:string
+    
     export let house:string
     export let index:number
     // let score: number = 0
 
     import { localStorageStore } from '@skeletonlabs/skeleton';
     	import type { Writable } from 'svelte/store';
+    import sapphire from '$lib/sapphireDark.JPG'
+	
+	import ruby  from '$lib/rubyDark.JPG' 
+	
+	import amber from '$lib/amberDark.JPG'
+	
+	import pearl from  '$lib/pearlDark.JPG'
+        
 
     const newScore: Writable<[number,number,number,number]> = localStorageStore('newStore', [0,0,0,0])
 
@@ -18,8 +26,20 @@
     <div class="container flex justify-center align-center rounded-3xl ">
         <button class=' hover:bg-green-400 flex items-center w-20 justify-center text-8xl bg-green-600 text-white rounded-3xl'on:click={()=> $newScore[index] += 1} >+</button>
         <a href="/" class="card !bg-transparent  rounded-3xl">
-            
-        <img src={message} alt="" srcset="" class= ' h-60 w-60 rounded-3xl opacity-90 object-fit' >
+            {#if house === 'ruby'}
+            <img src={ruby} alt="" srcset="" class= ' h-60 w-60 rounded-3xl opacity-90 object-fit' >
+            {/if}
+            {#if house === 'sapphire'}
+            <img src={sapphire} alt="" srcset="" class= ' h-60 w-60 rounded-3xl opacity-90 object-fit' >
+            {/if}
+            {#if house === 'amber'}
+            <img src={amber} alt="" srcset="" class= ' h-60 w-60 rounded-3xl opacity-90 object-fit' >
+            {/if}
+            {#if house === 'pearl'}
+            <img src={pearl} alt="" srcset="" class= ' h-60 w-60 rounded-3xl opacity-90 object-fit' >
+            {/if}
+
+        
         </a>
         <button class=' hover:bg-red-700 flex items-center w-20  justify-center text-9xl bg-red-900 text-white rounded-3xl'  on:click={()=> $newScore[index] -= 1} >-</button>
     
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,23 +5,16 @@
 	/* grid grid-rows-2 grid-flow-col gap-2*/
 	import Card from "$lib/Card.svelte";
 	
-	import sapphire from '$lib/sapphireDark.JPG'
-	
-	import ruby from '$lib/rubyDark.JPG'
-	
-	import amber from '$lib/amberDark.JPG'
-	
-	import pearl from  '$lib/pearlDark.JPG'
 
 </script>
 
 
 <div class="container h-full w-full mx-auto flex justify-center items-center overflow-hidden">
 	<div class=" grid grid-rows-2 grid-flow-col gap-10 h-90">
-		<Card message={ruby} house='ruby' index=0/>
-		<Card message={amber} house='amber' index=1/>
-		<Card message={pearl} house='pearl' index=2/>
-		<Card message={sapphire} house='sapphire' index=3/>
+		<Card  house='ruby' index=0/>
+		<Card  house='amber' index=1/>
+		<Card  house='pearl' index=2/>
+		<Card  house='sapphire' index=3/>
 		
 	</div>
 	
